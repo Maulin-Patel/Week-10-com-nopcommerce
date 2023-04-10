b61e822629741be5e6b5377d27682e6314a4bb50
Maulin-Patel <sareejunction@gmail.com>
2023-04-10 13:31:58 +0100

    Homework Week 10 com-nopcommerce

---
+++ src/ChromeBrowserTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+/**
+ * 1. Setup Chrome browser
+ * 2. Open URL https://demo.nopcommerce.com/login?returnUrl=%2F
+ * 3. Print the title of the page
+ * 4. Print the current url
+ * 5. Print the page source
+ * 6. Enter the email to email field
+ * 7. Enter the password to password field
+ * 8. Close the browser
+ */
+
+public class ChromeBrowserTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver(); // Object creation of WebDriver using ChromeDriver Class
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2"); // Opening provided url in Chrome Browser
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));// Telling browser to wait implicitly
+        System.out.println(driver.getTitle()); // Get title of the web page
+        System.out.println("The current URL of the page is: " + driver.getCurrentUrl()); // Getting and printing current url of the page
+        System.out.println("The Page source is: " + driver.getPageSource()); // Getting and printing current page source
+        driver.findElement(By.id("Email")).sendKeys("maulin1@gmail.com"); // Finding email field with id Locator and passing value to put into email field
+        driver.findElement(By.id("Password")).sendKeys("12345"); // // Finding password field with id Locator and passing value to put into password field
+        driver.close(); // Close the browser
+    }
+}
